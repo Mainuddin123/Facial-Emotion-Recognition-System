b09286f01a5ad98525338c3fafbbb28a464c5b8d
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2026-08-04 09:42:57 +0530

    Modify RTC configuration for STUN and TURN servers
    Updated RTC configuration to include new STUN and TURN servers with credentials.

--- app.py
+++ app.py
@@ -243,10 +243,28 @@ else:
         rtc_configuration={
             "iceServers": [
                 {
-                    "urls": [
-                        "stun:stun.l.google.com:19302"
-                    ]
-                }
+                    "urls": ["stun:stun.relay.metered.ca:80"],
+                },
+                {
+                    "urls": ["turn:global.relay.metered.ca:80"],
+                    "username": "d4a26fc7710d51300f0fc52d",
+                    "credential": "cibLU/J0Zvz5wiVZ",
+                },
+                {
+                    "urls": ["turn:global.relay.metered.ca:80?transport=tcp"],
+                    "username": "d4a26fc7710d51300f0fc52d",
+                    "credential": "cibLU/J0Zvz5wiVZ",
+                },
+                {
+                    "urls": ["turn:global.relay.metered.ca:443"],
+                    "username": "d4a26fc7710d51300f0fc52d",
+                    "credential": "cibLU/J0Zvz5wiVZ",
+                },
+                {
+                    "urls": ["turns:global.relay.metered.ca:443?transport=tcp"],
+                    "username": "d4a26fc7710d51300f0fc52d",
+                    "credential": "cibLU/J0Zvz5wiVZ",
+                },
             ]
         },
 
